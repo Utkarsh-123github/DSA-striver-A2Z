549d8b30a133de6efbe577af9fc343a038e899cb
Utkarsh Tiwari <utk0703@gmail.com>
2024-01-10 15:18:27 +0530

    min element in rotated sorted array

---
+++ Binary Search/08_minimum-element-in-rotated-sorted-array.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int findMin(vector<int>arr)
+{
+    int n = arr.size();
+    int low = 0 , high = n-1;
+    int mid = low + (high-low)/2;
+    int ans = INT_MAX;
+    while(low<=high)
+    {
+        if(arr[mid]>=arr[low]){
+            ans = min(ans,arr[low]);
+            low = mid+1;
+        }
+        else{
+            ans = min(ans,arr[mid]);
+            high = mid-1;
+        }
+        mid = low + (high-low)/2;
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int>a = {7,8,0,2,3};
+    cout<<findMin(a);
+    return 0;
+}
