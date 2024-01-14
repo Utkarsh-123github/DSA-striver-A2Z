2aa330f7e1a079e44168bc2caa98a71f89eaf02c
Utkarsh Tiwari <utk0703@gmail.com>
2024-01-14 15:37:36 +0530

    Largest subarray sum minimized BS

---
+++ Binary Search/21_Largest-subarray-sum-minimized.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int countPartitions(vector<int> &a, int maxSum) {
+    int n = a.size(); //size of array.
+    int partitions = 1;
+    long long subarraySum = 0;
+    for (int i = 0; i < n; i++) {
+        if (subarraySum + a[i] <= maxSum) {
+            //insert element to current subarray
+            subarraySum += a[i];
+        }
+        else {
+            //insert element to next subarray
+            partitions++;
+            subarraySum = a[i];
+        }
+    }
+    return partitions;
+}
+
+int largestSubarraySumMinimized(vector<int> &a, int k) {
+    int low = *max_element(a.begin(), a.end());
+    int high = accumulate(a.begin(), a.end(), 0);
+    //Apply binary search:
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int partitions = countPartitions(a, mid);
+        if (partitions > k) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return low;
+}
+
+int main()
+{
+    vector<int> a = {1,2,3,4,5};
+    int k = 3;
+    int ans = largestSubarraySumMinimized(a, k);
+    cout << "The answer is: " << ans << "\n";
+    return 0;
+}
