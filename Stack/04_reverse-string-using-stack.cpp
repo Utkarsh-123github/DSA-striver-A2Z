1d084e88fa4bab5880a621957e15dfcbaca6f56f
Utkarsh Tiwari <utk0703@gmail.com>
2024-02-06 22:04:23 +0530

    reverse string using stack

---
+++ Stack/04_reverse-string-using-stack.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string reverseString(string s)
+{
+    stack<char>st;
+    for(int i=0;i<s.length();i++)
+    {
+        st.push(s[i]);
+    }
+    string ans;
+    while(st.empty()==false)
+    {
+        ans = ans + st.top();
+        st.pop();
+    }
+    return ans;
+}
+
+int main()
+{
+    string s = "Utkarsh";
+    s = reverseString(s);
+    cout<<s<<endl;
+}
