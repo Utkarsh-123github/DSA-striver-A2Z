cbd343752b274b5c3ac8581a6cc1fe478bd347fa
Utkarsh Tiwari <utk0703@gmail.com>
2024-02-07 11:11:08 +0530

    redundant brackets stacks

---
+++ Stack/10_redundant-brackets.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+bool redundantBrackets(string &s)
+{
+    stack<char>st;
+    for(int i=0;i<s.length();i++)
+    {
+        char ch = s[i];
+        if(ch=='(' || ch=='+' || ch=='-' || ch=='/' || ch=='*')
+        {
+            st.push(ch);
+        }
+        else if(ch==')')
+        {
+            bool isRedundant = true;
+            while(st.top()!='(')
+            {
+                char top = st.top();
+                if(top=='+' || top=='-' || top=='*' || top=='/')
+                {
+                    isRedundant = false;
+                }
+                st.pop();
+            }
+            if(isRedundant == true)
+            {
+                return true;
+            }
+            st.pop();
+        }
+    }
+    return false;
+}
