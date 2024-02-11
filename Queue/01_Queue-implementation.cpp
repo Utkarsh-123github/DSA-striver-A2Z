6b9027c820eb413bcc701cfd68387cf844db58d4
Utkarsh Tiwari <utk0703@gmail.com>
2024-02-11 09:50:13 +0530

    Queue implementation using array

---
+++ Queue/01_Queue-implementation.cpp
@@ -0,0 +1,72 @@
+// Queue implementation using array
+#include<bits/stdc++.h>
+using namespace std;
+
+class Queue
+{
+    public:
+    int* arr;
+    int qfront;
+    int rear;
+    int size;
+    Queue()
+    {
+        size = 100001;
+        arr = new int[size];
+        qfront = 0;
+        rear = 0;
+    }
+
+    bool isEmpty()
+    {
+        if(qfront == rear)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    void enqueue(int data)
+    {
+        if(rear == size)
+        {
+            cout<<"Queue is full."<<endl;
+        }
+        else{
+            arr[rear] = data;
+            rear++;
+        }
+    }
+
+    int dequeue()
+    {
+        if(qfront == rear)
+        {
+            return -1;
+        }
+        else{
+            int ans = arr[qfront];
+            arr[qfront] = -1;
+            qfront++;
+            if(qfront == rear)
+            {
+                qfront = 0;
+                rear = 0;
+            }
+            return ans;
+        }
+    }
+
+    int front()
+    {
+        if(qfront == rear)
+        {
+            return -1;
+        }
+        else{
+            return arr[qfront];
+        }
+    }
+};
