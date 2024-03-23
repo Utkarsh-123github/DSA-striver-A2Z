60c5ea6646cd0fdbb3b6544ce6aad4e23c01e72e
Utkarsh Tiwari <utk0703@gmail.com>
2024-03-23 10:39:43 +0530

    vertical order traversal

---
+++ Binary Tree/12_vertical-order-traversal.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node
+{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+vector<int> VerticalOrderTraversal(Node* root)
+{
+    vector<int>ans;
+    queue<pair<Node* , pair<int , int>>>q;
+    // stores{node,{vertical,level}}
+
+    map<int , map<int , multiset<int>>>mp; 
+    // stores vertical,level,node->data
+    // multiset because it will handle the case where node->data will be same
+
+    q.push({root , {0,0}});
+    while(!q.empty())
+    {
+        auto p = q.front();
+        q.pop();
+        Node* node = p.first;
+        int x = p.second.first; // Vertical
+        int y = p.second.second; // Level
+        mp[x][y].insert(node->data);
+        // check left
+        if(node->left!= NULL)
+        {
+            q.push({node->left,{x-1,y+1}});
+        }
+        // check right
+        if(node->right!= NULL)
+        {
+            q.push({node->right,{x+1,y+1}});
+        }
+    }
+    for (auto it : mp) {
+
+        vector<int> level;
+
+        for (auto p : it.second) { // iterate for level,node->val;
+
+            level.insert(level.end(), p.second.begin(), p.second.end());
+        }
+
+        for (int v : level) {
+
+            ans.push_back(v);
+        }
+  }
+
+  return ans;
+}
